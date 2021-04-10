7b90e6766533ff7d654ecdae9d65c4231c760209
chu-young <ttheko101@gmail.com>
2021-04-10 18:10:53 +0200

    added relavent javascript example for news

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ fetch('https://api.nytimes.com/svc/topstories/v2/world.json?api-key=pi7mLJnxxrkm
 .then(res => res.json())
 .then(data =>{
     for(i = 0; i < 3; i++){
-        console.log(data.results[i].title)
-    }
-    
-})
+        
+        var exapmple = document.getElementById('example');
+    exapmple.innerHTML += `
+    <h3>${data.results[i].title}</h3>
+<p class='flow-text'>${data.results[i].abstract}</p>
+<p>~${data.results[i].byline}</p>
+    `;
+    };
+});
