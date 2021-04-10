d849a754f4cf4ddd75720493fdb6fd93c5d10058
chu-young <ttheko101@gmail.com>
2021-04-10 11:44:17 +0200

    added image

--- app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-    var collapsibleElem = document.querySelector('.collapsible');
-   var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-  });
