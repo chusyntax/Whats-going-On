027303bf7d2ec517f430eb538138b195d397c044
chu-young <ttheko101@gmail.com>
2021-04-17 18:27:46 +0200

    added first part of top stories section

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 fetch('https://api.nytimes.com/svc/topstories/v2/world.json?api-key=pi7mLJnxxrkmGObWp5KA3eD8ksQV6Tiy')
 .then(res => res.json())
 .then(data =>{
-    for(i = 0; i < 4; i++){
-        
-        var exapmple = document.getElementById('example');
-    exapmple.innerHTML += `
-    <h4>${data.results[i].title}</h4>
-<p class='flow-text'>${data.results[i].abstract}</p>
-<p>~${data.results[i].byline}</p>
-    `;
-    };
+console.log(data)
+    var topStory = document.getElementById('top-story');
+    var innerTopStory = `
+    <img class="right-align responsive-img" width="650" src='${data.results[0].multimedia[0].url}'>
+    <small>~${data.results[0].multimedia[0].caption}</small>
+    <h3 Word>${data.results[0].title}</h3>
+    <p class='flow-text'>${data.results[0].abstract}</p>
+    <p>~${data.results[0].byline}</p>
+    <span><a href='${data.results[0].url}' target="_blank"><em>Read More >>> </em></a></span>
+    `
+topStory.innerHTML += innerTopStory;
+
+
+
+    // for(i = 0; i < 4; i++){
+    //   var topStories = document.getElementById('top-stories');
+
+    // topStories.innerHTML +=` 
+    // <div></div>
+    // `;
+    
+    // };
 });
