5feb6837129c44aa15e29eed0255dd241ef3ae05
chu-young <ttheko101@gmail.com>
2021-04-17 15:40:07 +0200

    added image carouself for top stories

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 fetch('https://api.nytimes.com/svc/topstories/v2/world.json?api-key=pi7mLJnxxrkmGObWp5KA3eD8ksQV6Tiy')
 .then(res => res.json())
 .then(data =>{
-    for(i = 0; i < 3; i++){
+    for(i = 0; i < 4; i++){
         
         var exapmple = document.getElementById('example');
     exapmple.innerHTML += `
-    <h3>${data.results[i].title}</h3>
+    <h4>${data.results[i].title}</h4>
 <p class='flow-text'>${data.results[i].abstract}</p>
 <p>~${data.results[i].byline}</p>
     `;
