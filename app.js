9161e633cf69f28f64236c29e9813bf20c28ea86
chu-young <ttheko101@gmail.com>
2021-04-18 15:09:37 +0200

    completed top stories section

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ console.log(data)
 //Top Story Section
     var topStory = document.getElementById('top-story');
     var innerTopStory = `
-    <img class="right-align responsive-img" width="650" src='${data.results[0].multimedia[0].url}'>
+    
+    <img class="right-align responsive-img" src='${data.results[0].multimedia[0].url}'>
     <small>~${data.results[0].multimedia[0].caption}</small>
-    <h3 Word>${data.results[0].title}</h3>
+    <hr>
+    <h1>${data.results[0].title}</h1>
     <p class='flow-text'>${data.results[0].abstract}</p>
     <p>~${data.results[0].byline}</p>
     <span><a href='${data.results[0].url}' target="_blank"><em>Read More >>> </em></a></span>
+    <hr>
+
     `
 topStory.innerHTML += innerTopStory;
 
@@ -35,11 +39,6 @@ topStory.innerHTML += innerTopStory;
    <img class="right-align responsive-img" src='${data.results[3].multimedia[0].url}'>
    ${data.results[3].title}
    <small>~${data.results[3].byline}</small>
-   </li>
-   <li class="collection-item">
-   <img class="right-align responsive-img" src='${data.results[4].multimedia[0].url}'>
-   ${data.results[4].title}
-   <small>~${data.results[4].byline}</small>
    </li>
  </ul>
        
