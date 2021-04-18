b2931b2df31c211c80121fb11ff84137ca0b9963
chu-young <ttheko101@gmail.com>
2021-04-18 15:12:36 +0200

    added neccesary extra styling for top stories section

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 fetch('https://api.nytimes.com/svc/topstories/v2/world.json?api-key=pi7mLJnxxrkmGObWp5KA3eD8ksQV6Tiy')
 .then(res => res.json())
 .then(data =>{
-console.log(data)
 
 //Top Story Section
     var topStory = document.getElementById('top-story');
@@ -11,7 +10,9 @@ console.log(data)
     <small>~${data.results[0].multimedia[0].caption}</small>
     <hr>
     <h1>${data.results[0].title}</h1>
+    <br>
     <p class='flow-text'>${data.results[0].abstract}</p>
+    <br>
     <p>~${data.results[0].byline}</p>
     <span><a href='${data.results[0].url}' target="_blank"><em>Read More >>> </em></a></span>
     <hr>
@@ -23,6 +24,7 @@ topStory.innerHTML += innerTopStory;
 //Other Stories Section
    var otherStories = document.getElementById('other-top-stories');
    var innerOtherStories = `
+   
    <ul class="collection with-header">
    <li class="collection-header"><h3>Other Top Stories</h3></li>
    <li class="collection-item">
