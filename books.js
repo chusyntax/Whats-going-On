2d6f45333c76187a8fa04726c5fdfb4f0ada970e
chu-young <ttheko101@gmail.com>
2021-04-26 09:43:49 +0200

    changed a few styles and structures

--- books.js
+++ books.js
@@ -4,17 +4,20 @@ fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json
     console.log(data);
     var topRatedBook = document.getElementById('top-rated-book');
 var innerBook =`
- <div class="col s6 l6">
+<div class='row'>
+<div class="col s6 l6">
  <p><img class="right-align responsive-img" src='${data.results.books[0].book_image}'></p>
  </div>
 
 <div class="col s6 l6">
+<h1>Title: ${data.results.books[0].title}</h1>
 <h2>By: ${data.results.books[0].author}</h2>
+
+</div>
 </div>
 `
 topRatedBook.innerHTML += innerBook;
 
 
 
-
-})
+});
