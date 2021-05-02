21cf82e3c8e867c56875b2e2ec599622628d8a86
chu-young <ttheko101@gmail.com>
2021-05-02 16:26:36 +0200

    added other books structure

--- src/books.js
+++ src/books.js
@@ -1,9 +1,11 @@
-export function runBooksSection (){
+ function runBooksSection (){
     
     fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=pi7mLJnxxrkmGObWp5KA3eD8ksQV6Tiy')
 .then(res => res.json())
 .then(data =>{
     console.log(data);
+
+    //First book
     var topRatedBook = document.getElementById('top-rated-book');
 var innerTopBook =`
 <div class='row'>
@@ -27,9 +29,69 @@ var innerTopBook =`
 </div>
 `
 topRatedBook.innerHTML += innerTopBook;
+//rest of the books
 
+var otherBooks = document.getElementById('other-books');
+
+var innerOtherBooks =`
+<div class='row'>
+<div class="col s6 l4">
+<div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator responsive-img" style='width:100%' src='${data.results.books[1].book_image}'>
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+    </div>
+    </div>
+  </div>
+
+  <div class="col s6 l4">
+<div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator responsive-img" style='width:100%' src='${data.results.books[1].book_image}'>
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+    </div>
+    </div>
+  </div>
+
+  <div class="col s6 l4">
+  <div class="card">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator responsive-img" style='width:100%' src='${data.results.books[1].book_image}'>
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+        <p><a href="#">This is a link</a></p>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+      </div>
+      </div>
+    </div>
+
+
+</div>
+        
+`
+
+otherBooks.innerHTML += innerOtherBooks;
 
 
 });
 };
 
+runBooksSection();
