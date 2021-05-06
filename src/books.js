37f3f5a05c71d23f1a51fd09eca2e848b1e680d0
chu-young <ttheko101@gmail.com>
2021-05-06 17:45:28 +0200

    added buy now button to all books

--- src/books.js
+++ src/books.js
@@ -26,8 +26,8 @@ var innerTopBook =`
 
 <br>
 <br>
-<a class="waves-effect waves-light btn orange"><i class="material-icons right">book</i>Buy Now</a>
-<a class="waves-effect waves-light btn orange"><i class="material-icons right">attach_money</i>Buy Now</a>
+<a href="${data.results.books[0].amazon_product_url}" target="_blank" class="waves-effect waves-light btn orange"><i class="material-icons right">book</i>Buy Now</a>
+<a class="waves-effect waves-light btn orange">ISBN 10: ${data.results.books[0].primary_isbn10}</a>
 
 </div>
 </div>
@@ -49,7 +49,7 @@ var innerOtherBooks =
     </div>
     <div class="card-content">
       <span class="card-title activator grey-text text-darken-4 display-6">${data.results.books[1].title}<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
+      <p><a href="${data.results.books[1].amazon_product_url}" target="_blank" class="waves-effect waves-light btn orange"><i class="material-icons right">book</i>Buy Now</a></p>
     </div>
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">${data.results.books[1].title}<i class="material-icons right">close</i></span>
@@ -67,7 +67,7 @@ var innerOtherBooks =
     </div>
     <div class="card-content">
       <span class="card-title activator grey-text text-darken-4 display-6">${data.results.books[2].title}<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
+      <p><a href="${data.results.books[2].amazon_product_url}" target="_blank" class="waves-effect waves-light btn orange"><i class="material-icons right">book</i>Buy Now</a></p>
     </div>
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">${data.results.books[2].title}<i class="material-icons right">close</i></span>
@@ -79,13 +79,13 @@ var innerOtherBooks =
   </div>
 
   <div class="col s6 l3">
-<div class="card">
+<div class="card sticky-action">
     <div class="card-image waves-effect waves-block waves-light">
       <img class="activator"  src='${data.results.books[3].book_image}'>
     </div>
     <div class="card-content">
       <span class="card-title activator grey-text text-darken-4 display-6">${data.results.books[3].title}<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
+      <p><a href="${data.results.books[3].amazon_product_url}" target="_blank" class="waves-effect waves-light btn orange"><i class="material-icons right">book</i>Buy Now</a></p>
     </div>
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">${data.results.books[3].title}<i class="material-icons right">close</i></span>
@@ -103,7 +103,7 @@ var innerOtherBooks =
       </div>
       <div class="card-content">
         <span class="card-title activator grey-text text-darken-4 display-6">${data.results.books[4].title}<i class="material-icons right">more_vert</i></span>
-        <p><a href="#">This is a link</a></p>
+        <p><a href="${data.results.books[4].amazon_product_url}" target="_blank" class="waves-effect waves-light btn orange"><i class="material-icons right">book</i>Buy Now</a></p>
       </div>
       <div class="card-reveal">
         <span class="card-title grey-text text-darken-4">${data.results.books[4].title}<i class="material-icons right">close</i></span>
@@ -123,7 +123,7 @@ var innerOtherBooks =
     </div>
     <div class="card-content">
       <span class="card-title activator grey-text text-darken-4">${data.results.books[5].title}<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
+      <p><a href="${data.results.books[5].amazon_product_url}" target="_blank" class="waves-effect waves-light btn orange"><i class="material-icons right">book</i>Buy Now</a></p>
     </div>
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">${data.results.books[5].title}<i class="material-icons right">close</i></span>
@@ -141,7 +141,7 @@ var innerOtherBooks =
     </div>
     <div class="card-content">
       <span class="card-title activator grey-text text-darken-4">${data.results.books[6].title}<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
+      <p><a href="${data.results.books[6].amazon_product_url}" target="_blank" class="waves-effect waves-light btn orange"><i class="material-icons right">book</i>Buy Now</a></p>
     </div>
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">${data.results.books[6].title}<i class="material-icons right">close</i></span>
@@ -159,7 +159,7 @@ var innerOtherBooks =
     </div>
     <div class="card-content">
       <span class="card-title activator grey-text text-darken-4">${data.results.books[7].title}<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
+      <p><a href="${data.results.books[7].amazon_product_url}" target="_blank" class="waves-effect waves-light btn orange"><i class="material-icons right">book</i>Buy Now</a></p>
     </div>
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">${data.results.books[7].title}<i class="material-icons right">close</i></span>
@@ -177,7 +177,7 @@ var innerOtherBooks =
       </div>
       <div class="card-content">
         <span class="card-title activator grey-text text-darken-4">${data.results.books[8].title}<i class="material-icons right">more_vert</i></span>
-        <p><a href="#">This is a link</a></p>
+        <p><a href="${data.results.books[8].amazon_product_url}" target="_blank" class="waves-effect waves-light btn orange"><i class="material-icons right">book</i>Buy Now</a></p>
       </div>
       <div class="card-reveal">
         <span class="card-title grey-text text-darken-4">${data.results.books[8].title}<i class="material-icons right">close</i></span>
@@ -199,7 +199,7 @@ var innerOtherBooks =
     </div>
     <div class="card-content">
       <span class="card-title activator grey-text text-darken-4">${data.results.books[9].title}<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
+      <p><a href="${data.results.books[9].amazon_product_url}" target="_blank" class="waves-effect waves-light btn orange"><i class="material-icons right">book</i>Buy Now</a></p>
     </div>
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">${data.results.books[9].title}<i class="material-icons right">close</i></span>
@@ -217,7 +217,7 @@ var innerOtherBooks =
     </div>
     <div class="card-content">
       <span class="card-title activator grey-text text-darken-4">${data.results.books[10].title}<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
+      <p><a href="${data.results.books[10].amazon_product_url}" target="_blank" class="waves-effect waves-light btn orange"><i class="material-icons right">book</i>Buy Now</a></p>
     </div>
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">${data.results.books[10].title}<i class="material-icons right">close</i></span>
@@ -235,7 +235,7 @@ var innerOtherBooks =
     </div>
     <div class="card-content">
       <span class="card-title activator grey-text text-darken-4">${data.results.books[11].title}<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
+      <p><a href="${data.results.books[11].amazon_product_url}" target="_blank" class="waves-effect waves-light btn orange"><i class="material-icons right">book</i>Buy Now</a></p>
     </div>
     <div class="card-reveal">
       <span class="card-title grey-text text-darken-4">${data.results.books[11].title}<i class="material-icons right">close</i></span>
@@ -253,7 +253,7 @@ var innerOtherBooks =
       </div>
       <div class="card-content">
         <span class="card-title activator grey-text text-darken-4">${data.results.books[12].title}<i class="material-icons right">more_vert</i></span>
-        <p><a href="#">This is a link</a></p>
+        <p><a href="${data.results.books[12].amazon_product_url}" target="_blank" class="waves-effect waves-light btn orange"><i class="material-icons right">book</i>Buy Now</a></p>
       </div>
       <div class="card-reveal">
         <span class="card-title grey-text text-darken-4">${data.results.books[12].title}<i class="material-icons right">close</i></span>
