cc686a5ee443ec8b9a4ab1d2353e293e7d0e9cdd
chu-young <ttheko101@gmail.com>
2021-05-16 18:39:04 +0200

    added books section

--- src/movies.js
+++ src/movies.js
@@ -1,5 +1,9 @@
 fetch('https://api.nytimes.com/svc/movies/v2/critics/all.json?api-key=pi7mLJnxxrkmGObWp5KA3eD8ksQV6Tiy')
 .then(res => res.json())
 .then(data =>{
+
+    
     console.log(data);
+
+
 })
