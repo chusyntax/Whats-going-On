b27e20d29b5ef2ebb6cbe263e28a49eab93d4be2
chu-young <ttheko101@gmail.com>
2021-06-01 17:41:33 +0200

    added both reviewers

--- src/movies.js
+++ src/movies.js
@@ -1,27 +1,48 @@
-fetch('https://api.nytimes.com/svc/movies/v2/critics/all.json?api-key=pi7mLJnxxrkmGObWp5KA3eD8ksQV6Tiy')
+fetch('https://api.nytimes.com/svc/movies/v2/critics/full-time.json?api-key=pi7mLJnxxrkmGObWp5KA3eD8ksQV6Tiy')
 .then(res => res.json())
 .then(data =>{
  console.log(data);
 var reviewers = document.getElementById('reviewers');
 
-var innerReviewers = `
-<div class="card">
-    <div class="card-image waves-effect waves-block waves-light">
-      <img class="activator" src=${data.results[0].multimedia.resource.src}>
-    </div>
-    <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4">${data.results[0].display_name}<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
-    </div>
-    <div class="card-reveal">
-      <span class="card-title grey-text text-darken-4">${data.results[0].display_name}<i class="material-icons right">close</i></span>
-      <p>${data.results[0].bio}</p>
+    var innerReviewers = `
+    <div class='row'>
+    
+    <div class="col s12 l12">
+    <h2 class="header">${data.results[0].display_name}</h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src=${data.results[0].multimedia.resource.src}>
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>${data.results[0].bio}</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
     </div>
   </div>
-            
-`;
 
+  <div class="col s12 l12">
+  <h2 class="header">${data.results[2].display_name}</h2>
+  <div class="card horizontal">
+    <div class="card-image">
+      <img src=${data.results[2].multimedia.resource.src}>
+    </div>
+    <div class="card-stacked">
+      <div class="card-content">
+        <p>${data.results[2].bio}</p>
+      </div>
+      <div class="card-action">
+        <a href="#">This is a link</a>
+      </div>
+    </div>
+  </div>
+</div>
 
+    </div>
+    `;
 
 reviewers.innerHTML += innerReviewers;
 
